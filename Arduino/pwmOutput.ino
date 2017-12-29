93fff471939742d610c126d2d61571cb3020458a
Antonio Vásquez Briones <niox33@gmail.com>
2017-12-29 02:52:58 -0300

    file created

---
+++ Arduino/pwmOutput.ino
@@ -0,0 +1,11 @@
+void setup() {
+  
+  pinMode(9, OUTPUT);  //declaro el pin 9 como salida
+
+}
+
+void loop() {
+
+  analogWrite(9,100); //enciendo el pin 9 con un valor de potencia determinada 
+
+}
